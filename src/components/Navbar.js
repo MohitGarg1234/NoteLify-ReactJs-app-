79bf044e9e1d4cc36f59a17f8e2f5d99b97a6ded
Mohit Garg <mgarg1557@gmail.com>
2023-08-10 13:10:48 +0530

    commits

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -19,7 +19,7 @@ const Navbar = (props) => {
     
   <nav className="navbar navbar-expand-lg bg-dark navbar-dark ">
     <div className="container-fluid">
-        <Link className="navbar-brand" to="/">iNotebook</Link>
+        <Link className="navbar-brand" to="/">NoteLify</Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
         </button>
