9ebb85c1ef9de8d952b2a5727b3493edebde67d4
Mohit Garg <mgarg1557@gmail.com>
2024-06-01 15:49:42 +0530

    Some Changes for deploying

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -7,7 +7,7 @@ const SignUp = (props) => {
     const handleSubmit = async (e) => {
       e.preventDefault();
       if (credentials.password === credentials.cpassword) {
-        const response = await fetch("http://localhost:5000/api/auth/createuser", {
+        const response = await fetch("https://notelify-backend.onrender.com/api/auth/createuser", {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json'
